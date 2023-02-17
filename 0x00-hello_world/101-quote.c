f21c17bf450040618e3ff32d07fb23fd91c254b1
bereket-1983 <bereketm1983@gmail.com>
2023-02-17 00:52:24 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
-* main - A c program that prints a line to the standard error
-* Return 1 (success)
-*/
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
