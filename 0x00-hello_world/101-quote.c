4dc936af07263338a4b214ef08a3840a70598c25
bereket-1983 <bereketm1983@gmail.com>
2023-02-17 02:03:37 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<unistd.h>
+
 /**
 * main - A c program that prints a line to the standard error
-* Return: 1 (success)
+*
+* Return: Always 1 (success)
 */
 int main(void)
 	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
