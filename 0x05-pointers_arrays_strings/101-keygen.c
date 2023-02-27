934ea89fb4edf02f05be9d71cff3a882c9d17546
bereket-1983 <bereketm1983@gmail.com>
2023-02-27 11:41:23 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,33 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
+/**
+ * main - paso
+ *
+ * Return: int 0;
+ */
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	char str[100];
+	int i = 0, randNum = 0, suma = 0;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	srand (time(NULL));
+
+
+	for (i = 0; suma <= 2644; i++)
+	{
+		randNum = (rand() % 25) + 65;
+
+		str[i] = randNum;
+		suma = suma + randNum;
+	}
+
+	str[i++] = 2772 - suma;
+	str[i++] = '\0';
+
+	printf("%s\n", str);
+
+	return (0);
 }
 
